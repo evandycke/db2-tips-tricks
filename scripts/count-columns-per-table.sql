eb04db5af0ff23ccfeaaba3095e5cf4b96b9987d
Elie <23263163+evandycke@users.noreply.github.com>
2023-08-20 11:01:49 +0200

    Create count-columns-per-table.sql

---
+++ scripts/count-columns-per-table.sql
@@ -0,0 +1,16 @@
+-- Compter le nombre de colonnes par table dans une bibliothèque précise et/ou pour une liste de tables précise
+SELECT
+	SYSTEM_TABLE_SCHEMA,
+     SYSTEM_TABLE_NAME,
+     COUNT(*) AS COLUMNS_COUNT
+FROM 
+	QSYS2.SYSCOLUMNS
+WHERE 
+	(1=1) 
+	AND UPPER(SYSTEM_TABLE_SCHEMA) = 'IKGFFIC'
+	AND UPPER(SYSTEM_TABLE_NAME) in ('KINTIOP','KINTEOP','KPGROPP')
+GROUP BY
+	SYSTEM_TABLE_SCHEMA,
+    SYSTEM_TABLE_NAME
+ORDER BY  
+	1 ASC, 2 ASC;
